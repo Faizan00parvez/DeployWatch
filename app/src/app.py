cdb3bb892f8a4e7886dcf00526379e20e0da4cd2
Faizan Parvez <faizanparvez30@gmail.com>
2026-05-11 18:57:19 +0530

    feat: update app to v3.0

--- app/src/app.py
+++ app/src/app.py
@@ -9,7 +9,7 @@ def home():
     return jsonify({
         "app": "DeployWatch",
         "status": "running",
-        "message": "Automated deployment and monitoring system - v2.0 Live"
+        "message": "Automated deployment and monitoring system - v3.0 Live - Faizan"
     })
 
 @app.route('/health')
